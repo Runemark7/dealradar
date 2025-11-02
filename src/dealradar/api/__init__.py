3ae278aea334b5fa2bb3177aafc428dbb49322bb
Alexander Runemark <alexanderrunemark@gmail.com>
2025-11-02 22:40:08 +0100

    fix

--- src/dealradar/api/__init__.py
+++ src/dealradar/api/__init__.py
@@ -2,6 +2,6 @@
 Blocket API interaction module
 """
 
-from .scraper import fetch_blocket_api, fetch_search_results, fetch_multiple_listings
+from .scraper import fetch_blocket_api, fetch_search_results, fetch_multiple_listings, fetch_recent_search_results
 
-__all__ = ['fetch_blocket_api', 'fetch_search_results', 'fetch_multiple_listings']
+__all__ = ['fetch_blocket_api', 'fetch_search_results', 'fetch_multiple_listings', 'fetch_recent_search_results']
